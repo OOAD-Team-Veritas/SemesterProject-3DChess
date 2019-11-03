2301af7699b308310ead582c5e0708939a03a5cf
Riad Shash <riadshash@yahoo.com>
2019-11-02 19:58:52 -0600

    Initial commit with Board Logic & Simple factory & Basic scene setup

---
+++ OOAD Chess/Assets/Scripts/BoardLogic.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BoardLogic : MonoBehaviour
+{
+    //Lets make the tile size 1 by 1 units 1u^2 in area
+    private ChessPieceFactory pieceFactroy;
+    public GameObject positionTextObj;
+
+    /*
+     * Store the selected file location in 2D coordinates
+     * -1 means that nothing is selected
+     */
+    private int selectionTileX = -1;
+    private int selectionTileY = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Assign the script attached to the same gameObject
+        pieceFactroy = gameObject.GetComponent<ChessPieceFactory>();
+        CreateChessPieces();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        DrawChesBoard();
+        UpdateSelectedTile();
+    }
+
+    //Uses the simple factory -> ChessPieceFactory to place all the chess pieces
+    private void CreateChessPieces()
+    {
+        //Kings
+        pieceFactroy.CreateChessPiece("whiteKing",4,0,gameObject);
+        pieceFactroy.CreateChessPiece("blackKing",4,7,gameObject);
+
+        //Queens
+        pieceFactroy.CreateChessPiece("whiteQueen", 3, 0, gameObject);
+        pieceFactroy.CreateChessPiece("blackQueen", 3, 7, gameObject);
+
+        //Rooks
+        pieceFactroy.CreateChessPiece("whiteRook", 0, 0, gameObject);
+        pieceFactroy.CreateChessPiece("whiteRook", 7, 0, gameObject);
+        pieceFactroy.CreateChessPiece("blackRook", 0, 7, gameObject);
+        pieceFactroy.CreateChessPiece("blackRook", 7, 7, gameObject);
+
+        //Knights
+        pieceFactroy.CreateChessPiece("whiteKnight", 1, 0, gameObject);
+        pieceFactroy.CreateChessPiece("whiteKnight", 6, 0, gameObject);
+        pieceFactroy.CreateChessPiece("blackKnight", 1, 7, gameObject);
+        pieceFactroy.CreateChessPiece("blackKnight", 6, 7, gameObject);
+
+        //Bishops
+        pieceFactroy.CreateChessPiece("whiteBishop", 2, 0, gameObject);
+        pieceFactroy.CreateChessPiece("whiteBishop", 5, 0, gameObject);
+        pieceFactroy.CreateChessPiece("blackBishop", 2, 7, gameObject);
+        pieceFactroy.CreateChessPiece("blackBishop", 5, 7, gameObject);
+
+        //Pawns
+        for(int i = 0; i <=7; i++)
+        {
+            pieceFactroy.CreateChessPiece("whitePawn", i, 1, gameObject);
+            pieceFactroy.CreateChessPiece("blackPawn", i, 6, gameObject);
+        }
+    }
+
+    /*Debug preview of the concept board
+     * A chess board is 8x8 square tiles
+     */
+    private void DrawChesBoard()
+    {
+        //Width and Height vectors
+        Vector3 widthLine = new Vector3(1,0,0) * 8;
+        Vector3 heightLine = new Vector3(0,0,1) * 8;
+        Vector3 startVec;
+
+        //We are on the x/z plane
+        for (int i =0; i <= 8; i++)
+        {
+            //Draw the width lines one at a time (z/forward)
+            startVec = new Vector3(0,0,1) * i;
+            Debug.DrawLine(startVec, startVec + widthLine);
+            for(int j = 0; j <= 8; j++)
+            {
+                //Draw the height lines one at a time x/right varies)
+                startVec = new Vector3(1, 0, 0) * j;
+                Debug.DrawLine(startVec, startVec + heightLine);
+            }
+        }
+        //After that we have our 8x8 grid where the area of each is 1u^2
+
+        //Draw the selected tile
+
+        //Check if the we have a valid selection
+        if(selectionTileX >= 0 && selectionTileY >= 0)
+        {
+            //We have a selected tile so we draw an "X" in the tile!
+            Debug.DrawLine(
+                Vector3.forward * selectionTileY + Vector3.right * selectionTileX,
+                Vector3.forward * (selectionTileY + 1) + Vector3.right * (selectionTileX + 1));
+            Debug.DrawLine(
+                Vector3.forward * ( selectionTileY + 1 ) + Vector3.right * selectionTileX,
+                Vector3.forward * selectionTileY + Vector3.right * (selectionTileX + 1));
+        }
+    }
+
+    /*
+     * Since the grid starts at (0,0,0) position in world space, the coordinates of 
+     * each grid makes sense. 
+     * 
+     * Uses the Box collider located on ChessPlane object (LayerMask)
+     */
+    private void UpdateSelectedTile()
+    {
+        //Get the Layer of the ChessPlane used by the Raycasthit
+        LayerMask lm = LayerMask.GetMask("ChessPlane");
+
+        //Check if there is a camera... (we need it for what's next)
+        if (!Camera.main)
+            return;
+
+        RaycastHit mouseHit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out mouseHit, 50.0f, lm))
+        {
+            //Truncate the coordinates...
+            selectionTileX = (int)mouseHit.point.x;
+            selectionTileY = (int)mouseHit.point.z;
+
+            //Print the selection coordinates to the debug log console
+            positionTextObj.GetComponent<Text>().text = "Selected tile: [" + selectionTileX + " " + selectionTileY + "]";
+            Debug.Log("Selected tile: [" + selectionTileX + " " + selectionTileY + "]");
+        }
+        //Reset values is not a valid selection
+        else
+        {
+            selectionTileX = -1;
+            selectionTileY = -1;
+        }
+    }
+}
---
+++ OOAD Chess/Assets/Scripts/ChessPieceFactory.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessPieceFactory : MonoBehaviour
+{
+    //Contains all the chess pieces
+    public List<GameObject> chessPiecesPrefabs;
+    private const float TILEOFFSET = 0.5f;
+    private const float TILESIZE = 1.0f;
+
+    public GameObject CreateChessPiece(string type, int x, int y, GameObject parent)
+    {
+        GameObject newPiece = null;
+        Vector3 fixedPosition = GetTileCenter(x, y);
+
+        newPiece = Instantiate(chessPiecesPrefabs[GetIndex(type)],fixedPosition, Quaternion.identity) as GameObject;
+
+        //Make it a child of specified parent...
+        newPiece.transform.SetParent(parent.transform);
+        return newPiece;
+    }
+
+    //Helper function to get the index of the prefab object we want...
+    private int GetIndex(string type)
+    {
+        int index = 0;
+        switch (type)
+        {
+            case "whileKing":
+                index = 0;
+                break;
+            case "whiteQueen":
+                index = 1;
+                break;
+            case "whiteRook":
+                index = 2;
+                break;
+            case "whiteBishop":
+                index = 3;
+                break;
+            case "whiteKnight":
+                index = 4;
+                break;
+            case "whitePawn":
+                index = 5;
+                break;
+            case "blackKing":
+                index = 6;
+                break;
+            case "blackQueen":
+                index = 7;
+                break;
+            case "blackRook":
+                index = 8;
+                break;
+            case "blackBishop":
+                index = 9;
+                break;
+            case "blackKnight":
+                index = 10;
+                break;
+            case "blackPawn":
+                index = 11;
+                break;
+        }
+        return index;
+    }
+
+    //Helper function that finds the center of a file given the lower left position
+    private Vector3 GetTileCenter(int x, int y)
+    {
+        Vector3 centerOfTile = Vector3.zero;
+        centerOfTile.x += (TILESIZE * x) + TILEOFFSET;
+        centerOfTile.z += (TILESIZE * y) + TILEOFFSET;
+        return centerOfTile;
+    }
+}
