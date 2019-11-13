82512e74ea8831a8f0b1c88cac0813e17af34870
Riad Shash <riadshash@yahoo.com>
2019-11-13 16:32:26 -0700

    More work on timer & added more UI elements. Pause Menu

---
+++ OOAD Chess/Assets/Scripts/ChessGameTimer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Source for using string format
+ * https://answers.unity.com/questions/45676/making-a-timer-0000-minutes-and-seconds.html
+ */
+
+public class ChessGameTimer : MonoBehaviour
+{
+    private float gameTime, player1Time, player2Time;
+    
+    public GameObject gameTimerTxt;
+    public GameObject player1TimerTxt;
+    public GameObject player2TimerTxt;
+    public bool whiteTurn;
+    
+    void Start()
+    {
+        //Player 1 always goes first
+        whiteTurn = true;
+        gameTime = 0;
+        player1Time = 0;
+        player2Time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //time in seconds since the last frame
+        gameTime += Time.deltaTime;
+        gameTimerTxt.GetComponent<Text>().text = formatTimeString(gameTime);
+
+        if (whiteTurn)
+        {
+            player1Time += Time.deltaTime;
+            player1TimerTxt.GetComponent<Text>().text = formatTimeString(player1Time);
+        }
+        else
+        {
+            player2Time += Time.deltaTime;
+            player2TimerTxt.GetComponent<Text>().text = formatTimeString(player2Time);
+        }
+    }
+
+    private string formatTimeString(float time)
+    {
+        int minutes, seconds;
+        minutes = Mathf.FloorToInt(time / 60f);
+        seconds = Mathf.FloorToInt(time - minutes * 60);
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
---
+++ OOAD Chess/Assets/UIEvents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEvents : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void showPauseMenuAndPause()
+    {
+        //Pause the game
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void hidePauseMenuAndResume()
+    {
+        //Resume the game
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
