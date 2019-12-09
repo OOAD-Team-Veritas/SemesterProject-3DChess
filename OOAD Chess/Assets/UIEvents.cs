c9d050b34d0a5b18072a579bc60fe1960c99096f
Elizabeth Robinson <eliz.robinson8@gmail.com>
2019-12-08 18:31:31 -0700

    UI changes

--- OOAD Chess/Assets/UIEvents.cs
+++ OOAD Chess/Assets/UIEvents.cs
@@ -7,15 +7,23 @@ using UnityEngine.SceneManagement;
 public class UIEvents : MonoBehaviour
 {
     public GameObject pauseMenu;
-    //public GameObject startMenu;
+    public string playerOneName;
+    public string playerTwoName;
 
+    public void setPlayerOneName(string textPlayer1)
+    {
+        playerOneName = textPlayer1;
+    }
+    public void setPlayerTwoName(string textPlayer2)
+    {
+        playerTwoName = textPlayer2;
+    }
     public void showPauseMenuAndPause()
     {
         //Pause the game
         Time.timeScale = 0;
         pauseMenu.SetActive(true);
     }
-
     public void hidePauseMenuAndResume()
     {
         //Resume the game
@@ -30,8 +38,32 @@ public class UIEvents : MonoBehaviour
     }
     public void startGame()
     {
+        SceneManager.LoadScene("SetPlayerScene");
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("SetPlayerScene"));
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("StartScene"));
+    }
+    public void hideStartMenuAndLoadCreditsMenu()
+    {
+        SceneManager.LoadScene("CreditsScene");
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("CreditsScene"));
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("StartScene"));
+    }
+    public void hideCreditsMenuAndLoadStartMenu()
+    {
+        SceneManager.LoadScene("StartScene");
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("StartScene"));
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("CreditsScene"));
+    }
+    public void hideSetPlayerMenuAndLoadGame()
+    {
         SceneManager.LoadScene("MainScene");
         SceneManager.SetActiveScene(SceneManager.GetSceneByName("MainScene"));
-        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("StartScene"));
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("SetPlayerScene"));
+    }
+    public void hideSetPlayerMenuAndLoadStartMenu()
+    {
+        SceneManager.LoadScene("StartScene");
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("StartScene"));
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("SetPlayerScene"));
     }
 }
